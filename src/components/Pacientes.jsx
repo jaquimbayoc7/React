cd0c48603c2715d44aa7022da85b43ed29096b4b
BrayanMedina96 <alvaro.alarcon@corhuila.edu.co>
2023-01-17 17:38:55 -0500

    finalizado frontend

---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,19 @@
+
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+          <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}<span className="font-normal normal-case">Trosky</span></p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}<span className="font-normal normal-case">Julian Quimbayo</span></p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}<span className="font-normal normal-case">Correo@correo.com.co</span></p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de Ingreso:{' '}<span className="font-normal normal-case">17 de Enero de 2023</span></p>
+
+          <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas:{' '}<span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus cumque, ad quibusdam sed magnam iusto? Obcaecati deleniti quidem quod commodi ipsam rerum inventore asperiores facilis fugit cupiditate! Nobis tenetur veritatis voluptatem hic repudiandae! Fugit id ab sit cupiditate, dolorum delectus.</span></p>
+          
+    </div>
+  )
+}
+
+export default Pacientes

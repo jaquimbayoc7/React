f6c7d117c973b6fc3557591e5854ed0f113190c3
BrayanMedina96 <alvaro.alarcon@corhuila.edu.co>
2023-01-19 18:05:03 -0500

    cambios en formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,28 +2,54 @@ import { useState, useEffect } from "react";
 
 const Formulario = () => {
   const [nombre, setNombre] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fingreso, setFingreso] = useState('');
+  const [sintomas, setSintomas] = useState('');
 
-  //setNombre('Hook')
+  //Manejo de errores
+  const[error, setError]= useState(false);
 
-  //console.log(nombre);
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+    //validar formulario
+    if([nombre, propietario, email, fingreso, sintomas].includes('')){
+      console.log('Hay al menos un campo vacío');
+      setError(true);
+      return;
+    }else{
+      console.log('Todos los campos estas llenos');
+    }
+
+    //setError(false)
+
+    console.log('Enviando formulario');
+  }
 
 
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       <p className="text-lg mt-5 text-center mb-10">
         Añade Pacientes y {' '}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" onSubmit={handleSubmit}>
+        {error && (<div className="bg-red-800 text-white p-3 uppercase text-center font-bold rounded-md">
+
+            <p>Todos los campos son obligatorios!</p>
+            
+            </div>)}
+        
         <div className="mb-5">
 
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
             Nombre Mascota:
           </label>
           <input id="mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-          type="text" placeholder="Nombre de la mascota" value={nombre} onChange={(e)=> console.log(e.target.value)}/>
+          type="text" placeholder="Nombre de la mascota" value={nombre} onChange={(e)=> setNombre(e.target.value)}/>
           
         </div>
         <div className="mb-5">
@@ -32,7 +58,7 @@ const Formulario = () => {
             Nombre Propietario:
           </label>
           <input id="propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-          type="text" placeholder="Nombre del propietario"/>
+          type="text" placeholder="Nombre del propietario" value={propietario} onChange={(e)=> setPropietario(e.target.value)}/>
           
         </div>
 
@@ -42,7 +68,7 @@ const Formulario = () => {
             Email Propietario:
           </label>
           <input id="email" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-          type="email" placeholder="Email del propietario"/>
+          type="email" placeholder="Email del propietario" value={email} onChange={(e)=> setEmail(e.target.value)}/>
           
         </div>
         <div className="mb-5">
@@ -51,7 +77,7 @@ const Formulario = () => {
             Fecha de Ingreso:
           </label>
           <input id="fecha" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-          type="date"/>
+          type="date" value={fingreso} onChange={(e)=> setFingreso(e.target.value)}/>
           
         </div>
         <div className="mb-5">
@@ -60,7 +86,7 @@ const Formulario = () => {
             Síntomas:
           </label>
           <textarea className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-          id="sintomas"  placeholder="Describe los síntomas" />
+          id="sintomas"  placeholder="Describe los síntomas" value={sintomas} onChange={(e)=> setSintomas(e.target.value)} />
           
         </div>
 
--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,7 +1,7 @@
 
 const Pacientes = () => {
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
           <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}<span className="font-normal normal-case">Trosky</span></p>
 
           <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}<span className="font-normal normal-case">Julian Quimbayo</span></p>
